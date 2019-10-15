193473f25f77a5790947f1b081a09aafb23e40dc
Vlad <salabunv@ukr.net>
2019-10-16 02:42:52 +0300

    lb

--- lifemap-image-lightbox-by-vlad/image-lightbox.php
+++ lifemap-image-lightbox-by-vlad/image-lightbox.php
@@ -66,6 +66,29 @@ Author URI: http://salabun.com/
                     // Обертаю зображення у посилання для лайтбокса:
                     $tmpString = '<a href="'.$value->src.'"><img id="lm_lightbox" src="'.$value->src.'" alt="journey_thumbnail" class="zz"></a>';
                     
+/*
+                    $tmpString = '<a href="'.$value->src.'" data-toggle="modal" data-target="#modal_image_'.$id.'"><img src="'.$value->src.'" class="zz"></a>
+                    <!-- Modal -->
+<div class="modal fade" id="modal_image_'.$id.'" tabindex="-1" role="dialog" aria-labelledby="modal_image_'.$id.'_Title" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <img src="'.$value->src.'">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+                    ';
+*/
                     // Заміняю:
                     $html = str_replace($value->outertext,$tmpString, $html);
 
